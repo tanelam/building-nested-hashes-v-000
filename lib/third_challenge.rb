c62b2060a70a679802a595541f60e4df44968f08
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-17 19:18:21 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,13 +1,20 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => { 
+        name: "Lord Montague",
+        age: "53" 
         #your key/value pairs here
         },
       :matriarch => {
+        name: "Lady Montague"
+        age: "54"
         #your key/value pairs here
         },
       :hero => {
+        name: "Romeo"
+        age: "15"
+        status: "alive"
         #your key/value pairs here
         },
       :hero_friends => []
